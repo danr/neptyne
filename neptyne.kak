88ae39bb676253ed45088e7b5978fc58a099bcf1
Dan Rosén <dan.rosen@gu.se>
2019-06-26 15:49:30 +0200

    Jedi integration

--- neptyne.kak
+++ neptyne.kak
@@ -1,35 +1,58 @@
 
-map global insert <a-c> '<a-;>: neptyne_setup; neptyne_complete<ret>'
-map global insert <a-h> '<a-;>: neptyne_inspect<ret>'
+map global insert <a-C> '<a-;>: neptyne_setup; neptyne_complete<ret>'
+map global insert <a-i> '<a-;>: neptyne_setup; neptyne_jedi icomplete<ret>'
+map global insert <a-c> '<a-;>: neptyne_setup; neptyne_jedi complete<ret>'
+map global insert <a-d> '<a-;>: neptyne_setup; neptyne_jedi docstring<ret>'
+map global insert <a-z> '<a-;>: neptyne_setup; neptyne_jedi usages<ret>'
+map global insert <a-s> '<a-;>: neptyne_setup; neptyne_jedi sig<ret>'
+map global insert <a-g> '<a-;>: neptyne_setup; neptyne_jedi goto<ret>'
+map global insert <a-h> '<a-;>: neptyne_inspect normal<ret>'
 map global insert <a-w> '<a-;>: write<ret>'
 map global normal <a-h> ': neptyne_inspect<ret>'
 
 def neptyne_setup %{
     try %{
+        decl -hidden str _neptyne_tmp
         decl completions neptyne_completions
     }
     set -add window completers option=neptyne_completions
-    map window insert <a-c> '<a-;>: neptyne_complete<ret>'
+    map window insert <a-c> '<a-;>: neptyne_jedi complete<ret>'
 }
-def neptyne_complete %{
+
+def neptyne_jedi -params 1 %{
     eval -draft -no-hooks %{
-        try %{
-            exec h <a-i> w <a-:> '<a-;>;'
-        } catch %{
-            exec l
-        }
-        exec Gg <a-:>
-        echo -to-file .requests "complete %val{cursor_byte_offset} %val{client} %val{session} set window neptyne_completions %val{cursor_line}.%val{cursor_column}@%val{timestamp}
+        set window _neptyne_tmp "%val{cursor_line} %val{cursor_column} neptyne_completions %val{timestamp}"
+        exec \%
+        echo -to-file .requests "jedi_%arg{1} %val{cursor_byte_offset} %val{client} %val{session} %opt{_neptyne_tmp}
 %val{selection}"
     }
 }
-def neptyne_inspect %{
+
+def neptyne_complete %{
+    eval -draft -no-hooks %{
+        eval -draft %{
+            # first find the completion start
+            try %{
+                eval -draft %{
+                    exec h <a-k> [\w.] <ret> l <a-/> [\w.]* <ret> <a-:> <a-\;> \;
+                }
+            }
+            # set window _neptyne_tmp "set window neptyne_completions %val{cursor_line}.%val{cursor_column}@%val{timestamp}"
+            set window _neptyne_tmp "%val{cursor_line} %val{cursor_column} neptyne_completions %val{timestamp}"
+        }
+        # select everything backwards from the actual cursor position
+        exec Gg <a-:>
+        echo -to-file .requests "complete %val{cursor_byte_offset} %val{client} %val{session} %opt{_neptyne_tmp}
+%val{selection}"
+    }
+}
+def neptyne_inspect -params 1 %{
     eval -draft -no-hooks %{
         try %{
             exec <a-i> w <a-:> ';'
         }
         exec Gg <a-:>
-        echo -to-file .requests "inspect %val{cursor_byte_offset} %val{client} %val{session} %val{window_width} %val{window_height}
+        echo -to-file .requests "inspect %val{cursor_byte_offset} %val{client} %val{session} %arg{1} %val{window_width} %val{window_height}
 %val{selection}"
     }
 }
--- neptyne.py
+++ neptyne.py
@@ -1,6 +1,8 @@
 import jupyter_client as jc
 import hashlib
 import re
+import inspect
+import json
 
 from pprint import pprint
 from itertools import zip_longest
@@ -89,6 +91,16 @@ class dotdict(dict):
     __setattr__ = dict.__setitem__
     __delattr__ = dict.__delitem__
 
+def dbg(part):
+    def dbg_(line):
+        if line.strip().startswith('##'):
+            words = line.split()[1:]
+            words = ', '.join(f"'{word}': {word}" for word in words)
+            words = '{' + words + '}'
+            line = line.replace('##', f'dbg({words}) #')
+        return line
+    return '\n'.join(map(dbg_, part.split('\n')))
+
 @contextmanager
 def kernel(kernel_name='python'):
     with jc.run_kernel(kernel_name=kernel_name, stdin=False) as kc:
@@ -102,6 +114,7 @@ def kernel(kernel_name='python'):
                     yield 'interrupted'
                     continue
                 msg = msg['content']
+                # pprint(msg)
                 if msg.get('execution_state', None) == 'idle':
                     break
                 else:
@@ -112,6 +125,7 @@ def kernel(kernel_name='python'):
         def complete(i, offset, self):
             msg_id = kc.complete(i, offset)
             data = kc.get_shell_msg()['content']
+            # pprint(data)
             matches = data['matches']
             yield matches
             list(wait_idle())
@@ -127,68 +141,72 @@ def kernel(kernel_name='python'):
                 yield str(data)
             list(wait_idle())
 
+        def process_part(part, self):
+            self.executing(part)
+            msg_id = kc.execute(part)
+            cancel = False
+            for msg in wait_idle():
+                if 'data' in msg:
+                    # png and html can be sent here inline
+                    # pprint(msg)
+                    data = msg['data']
+                    png = data.get('image/png')
+                    svgxml = data.get('image/svg+xml')
+                    if png or svgxml:
+                        import libsixel
+                        from libsixel.encoder import Encoder
+                        import base64
+                        name = '/tmp/img.png'
+                        if svgxml:
+                            tmp = '/tmp/img.xml'
+                            with open(tmp, 'w') as f:
+                                f.write(svgxml)
+                            Popen(["convert", tmp, name]).wait()
+                        else:
+                            with open(name, 'wb') as f:
+                                f.write(base64.b64decode(png))
+                        enc = Encoder()
+                        # enc.setopt(e.SIXEL_OPTFLAG_WIDTH, "400")
+                        # enc.setopt(e.SIXEL_OPTFLAG_QUALITY, "low")
+                        enc.setopt(libsixel.SIXEL_OPTFLAG_COLORS, "256")
+                        if msg.get('metadata', {}).get('needs_background') == 'light':
+                            enc.setopt(libsixel.SIXEL_OPTFLAG_BGCOLOR, "#fff")
+                        enc.encode(name)
+                    elif 'text/plain' in data:
+                        text = data['text/plain']
+                        self.text(text)
+                    else:
+                        print('hmm?', data.keys())
+                elif msg == 'interrupted':
+                    cancel = True
+                elif 'ename' in msg:
+                    cancel = self.error(**msg) #ename, evalue, traceback
+                elif msg.get('name') in ['stdout', 'stderr']:
+                    self.stream(msg['text'], msg['name'])
+            msg = kc.get_shell_msg(timeout=1)
+            msg = msg['content']
+            for payload in msg.get('payload', []):
+                if 'data' in payload:
+                    # ?print
+                    data = payload['data']['text/plain']
+                    self.immediate(data)
+            return cancel
+
         def process(i, self):
             nonlocal prevs
-            parts = re.split(r'\n\n(?=\S)', i)
+            parts = [ dbg(p) for p in re.split(r'\n\n(?=\S)', i) ]
             zipped = list(zip_longest(parts, prevs))
             same, zipped = span(lambda part, prev: trim(prev) == trim(part), zipped)
             prevs = list(map(lambda x: x[0], same))
             for part, prev in zipped:
                 if part:
-                    self.executing(part)
-                    msg_id = kc.execute(part)
-                    cancel = False
-                    for msg in wait_idle():
-                        if 'data' in msg:
-                            # png and html can be sent here inline
-                            # pprint(msg)
-                            data = msg['data']
-                            png = data.get('image/png')
-                            svgxml = data.get('image/svg+xml')
-                            if png or svgxml:
-                                import libsixel
-                                from libsixel.encoder import Encoder
-                                import base64
-                                name = '/tmp/img.png'
-                                if svgxml:
-                                    tmp = '/tmp/img.xml'
-                                    with open(tmp, 'w') as f:
-                                        f.write(svgxml)
-                                    Popen(["convert", tmp, name]).wait()
-                                else:
-                                    with open(name, 'wb') as f:
-                                        f.write(base64.b64decode(png))
-                                enc = Encoder()
-                                # enc.setopt(e.SIXEL_OPTFLAG_WIDTH, "400")
-                                # enc.setopt(e.SIXEL_OPTFLAG_QUALITY, "low")
-                                enc.setopt(libsixel.SIXEL_OPTFLAG_COLORS, "256")
-                                if msg.get('metadata', {}).get('needs_background') == 'light':
-                                    enc.setopt(libsixel.SIXEL_OPTFLAG_BGCOLOR, "#fff")
-                                enc.encode(name)
-                            elif 'text/plain' in data:
-                                text = data['text/plain']
-                                self.text(text)
-                            else:
-                                print('hmm?', data.keys())
-                        elif msg == 'interrupted':
-                            cancel = True
-                        elif 'ename' in msg:
-                            cancel = self.error(**msg) #ename, evalue, traceback
-                        elif msg.get('name') in ['stdout', 'stderr']:
-                            self.stream(msg['text'], msg['name'])
-                    msg = kc.get_shell_msg(timeout=1)
-                    msg = msg['content']
-                    for payload in msg.get('payload', []):
-                        if 'data' in payload:
-                            # ?print
-                            data = payload['data']['text/plain']
-                            self.immediate(data)
+                    cancel = process_part(part, self)
                     if cancel:
                         break
                     else:
                         prevs.append(part)
 
-        yield dotdict(process=process, inspect=inspect, complete=complete)
+        yield dotdict(process=process, inspect=inspect, complete=complete, process_part=process_part)
 
 class Handler(dict):
     def __getattr__(self, k):
@@ -260,18 +278,29 @@ def test():
                 for msg in k[cmd](s, len(s), std):
                     print(msg)
 
+def filter_completions(xs):
+    normal = any(not x.name.startswith('_') for x in xs)
+    if normal:
+        return [x for x in xs if not x.name.startswith('_')]
+    else:
+        return xs
+
+def info(s):
+    return 'info -style menu "' + qq(s) + '"'
+
 if __name__ == '__main__':
     import sys
-    from inotify.adapters import Inotify
     if sys.argv[1:2] == ['test']:
         test()
     else:
+        from inotify.adapters import Inotify
         i = Inotify()
         i.add_watch('.')
         watched_file = sys.argv[1]
         import os
         common = os.path.commonpath([os.getcwd(), watched_file])
         watched_file = watched_file[1+len(common):]
+        jedi_setup = False
         # print(common, watched_file)
         with kernel('julia-1.1' if watched_file.endswith('.jl') else 'python') as k:
             try:
@@ -279,47 +308,141 @@ if __name__ == '__main__':
             except FileNotFoundError:
                 pass
             for event in i.event_gen(yield_nones=False):
-                (_, event_type, _, filename) = event
-                # print(event_type, filename)
+                try:
+                    (_, event_type, _, filename) = event
+                    # print(event_type, filename)
 
-                if event_type == ['IN_CLOSE_WRITE'] and filename == watched_file:
-                    k.process(open(watched_file, 'r').read(), std)
+                    if event_type == ['IN_CLOSE_WRITE'] and filename == watched_file:
+                        k.process(open(watched_file, 'r').read(), std)
 
-                if event_type == ['IN_CLOSE_WRITE'] and filename == '.requests':
-                    try:
-                        request, body = open(filename, 'r').read().split('\n', 1)
-                        cmd, pos, client, session, *args = request.split(' ')
-                        pos = int(pos)
-                    except ValueError:
-                        print('Invalid request:', str(open(filename, 'r').read()))
-                        continue
+                    if event_type == ['IN_CLOSE_WRITE'] and filename == '.requests':
+                        try:
+                            request, body = open(filename, 'r').read().split('\n', 1)
+                            cmd, pos, client, session, *args = request.split(' ')
+                            pos = int(pos)
+                        except ValueError:
+                            print('Invalid request:', str(open(filename, 'r').read()))
+                            continue
 
-                    def send(msg):
-                        msg = 'eval -client {} "{}"'.format(client, qq(msg))
-                        p = Popen(['kak', '-p', str(session).rstrip()], stdin=PIPE)
-                        p.stdin.write(msg.encode())
-                        p.stdin.flush()
-                        p.stdin.close()
-                        p.wait()
+                        def send(msg):
+                            msg = 'eval -client {} "{}"'.format(client, qq(msg))
+                            p = Popen(['kak', '-p', str(session).rstrip()], stdin=PIPE)
+                            p.stdin.write(msg.encode())
+                            p.stdin.flush()
+                            p.stdin.close()
+                            p.wait()
 
-                    if cmd == 'complete':
-                        for matches in k.complete(body, pos, std):
-                            msg = ['"' + qq(completion_esc(m)) + '|neptyne_inspect|' + qq(completion_esc(m)) + '"' for m in matches]
-                            msg = ' '.join(args + msg)
+                        def completions(matches, args):
+                            line, column = map(int, args[:2])
+                            _, _, neptyne_completions, timestamp = args
+                            matches = filter_completions(list(map(dotdict, matches)))
+                            if not matches:
+                                return
+                            qqc = lambda m: qq(completion_esc(m))
+                            msg = ['"' + qqc(m.name) + '|' + qqc(m.doccmd or info(m.docstring)) + '|' + qqc(m.name) + '"' for m in matches]
+                            m0 = matches[0]
+                            dist = len(m0.name) - len(m0.complete)
+                            cmd = ['set window', neptyne_completions, str(line) + '.' + str(column - dist) + '@' + timestamp]
+                            msg = ' '.join(cmd + msg)
                             send(msg)
 
-                    elif cmd == 'inspect':
-                        for text in k.inspect(body, pos, std):
-                            print('\n' + text)
-                            width, height = map(int, args)
-                            msg = unansi(text)
-                            msg = [line[:width-8] for line in msg.split('\n')]
-                            msg = '\n'.join(msg[:height-8])
-                            msg = 'info "{}"'.format(qq(msg))
-                            send(msg)
+                        if cmd == 'complete':
+                            for reply in k.complete(body, pos, std):
+                                matches = [
+                                    dict(name=m, complete=m, doccmd='neptyne_inspect menu')
+                                    for m in reply
+                                ]
+                                completions(matches, args)
 
-                    else:
-                        print('Invalid command: ', cmd)
+                        elif cmd == 'inspect':
+                            for text in k.inspect(body, pos, std):
+                                print('\n' + text)
+                                where, *args = args
+                                width, height = map(int, args)
+                                msg = unansi(text)
+                                msg = [line[:width-8] for line in msg.split('\n')]
+                                msg = '\n'.join(msg[:height-8])
+                                style = '-style menu' if where == 'menu' else ''
+                                msg = f'info {style} "{qq(msg)}"'
+                                send(msg)
+
+                        elif cmd == 'jedi_icomplete':
+
+                            # We run the jedi command inside the kernel
+
+                            line, column = map(int, args[:2])
+
+                            def _doit(*args, **kws):
+                                import jedi
+                                import json
+                                i = jedi.Interpreter(*args, **kws)
+                                cs = i.completions()
+                                d = [dict(name=c.name, complete=c.complete, docstring=c.docstring()) for c in cs]
+                                print(json.dumps(d))
+
+                            def on_reply(s, _fd):
+                                try:
+                                    matches = json.loads(s)
+                                except:
+                                    return
+                                completions(matches, args)
+
+                            doit_s = inspect.getsource(_doit)
+                            h = Handler()
+                            h.stream = on_reply
+                            h.text = lambda *_: None
+                            h.immediate = lambda *_: None
+                            h.executing = lambda *_: None
+
+                            # zap lines above current blob (to preserve line number)
+                            lines = []
+                            ok = True
+                            for l in body.split('\n')[:line][::-1]:
+                                if not l:
+                                    ok = False
+                                if ok:
+                                    lines.append(l)
+                                else:
+                                    lines.append('')
+                            body = '\n'.join(lines[::-1])
+
+                            k.process_part(f'{doit_s}\n_doit({body!r}, namespaces=[locals(), globals()], line={line}, column={column-1})', h)
+
+
+                        elif cmd.startswith('jedi'):
+                            cmd = cmd[len('jedi_'):]
+                            import jedi
+                            line, column = map(int, args[:2])
+                            try:
+                                script = jedi.Script(body, line, column - 1, watched_file)
+                            except:
+                                print('jedi failed')
+                                continue
+                            if cmd == 'complete':
+                                matches = [
+                                    dict(name=m.name, complete=m.complete, docstring=m.docstring())
+                                    for m in script.completions()
+                                ]
+                                completions(matches, args)
+                            elif cmd == 'docstring':
+                                try:
+                                    print(script.goto_definitions()[0].docstring())
+                                except:
+                                    print('nothing')
+                                    pass
+                            elif cmd == 'usages':
+                                pprint(script.usages())
+                            elif cmd == 'sig':
+                                pprint(script.call_signatures())
+                            elif cmd == 'goto':
+                                pprint(script.goto_definitions())
+                            else:
+                                print('Invalid jedi command: ', cmd)
+                        else:
+                            print('Invalid command: ', cmd)
+                except:
+                    import traceback as tb
+                    tb.print_exc()
 
 
 
