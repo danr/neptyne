989c6d7e13c17c92a2c11b53adfa2bcd20f6e6a5
Dan Rosén <dan.rosen@nbis.se>
2020-02-12 23:45:18 +0100

    Remove unused util function

--- utils.py
+++ utils.py
@@ -1,15 +1,13 @@
+class dotdict(dict):
+    __getattr__ = dict.get
+    __setattr__ = dict.__setitem__
+    __delattr__ = dict.__delitem__
 
-async def async_lambda(f):
-    return f()
 
 async def aseq(*futs):
     for fut in futs:
         await fut
 
-class dotdict(dict):
-    __getattr__ = dict.get
-    __setattr__ = dict.__setitem__
-    __delattr__ = dict.__delitem__
 
 def id_stream(prevs=()):
     id = max([0] + [prev.id or 0 for prev in prevs])
@@ -18,5 +16,4 @@ def id_stream(prevs=()):
         id += 1
         return id
     return bump
-
 
